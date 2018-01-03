2e55337cdd4db91475e85cbdf325aec391a41366
Said Tahsin Dane <tasomaniac@gmail.com>
2018-01-03 19:35:48 +0300

    Start ConfigureActivity on click

--- app/src/main/java/com/tasomaniac/devdrawer/main/WidgetViewHolder.kt
+++ app/src/main/java/com/tasomaniac/devdrawer/main/WidgetViewHolder.kt
@@ -1,7 +1,10 @@
 package com.tasomaniac.devdrawer.main
 
+import android.appwidget.AppWidgetManager
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.tasomaniac.devdrawer.configure.ConfigureActivity
 import com.tasomaniac.devdrawer.widget.WidgetNameResolver
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.main_item_widget.*
@@ -16,6 +19,12 @@ class WidgetViewHolder(
     mainWidgetTitle.text = widgetNameResolver.resolve(data.widget)
     mainWidgetAppList.adapter = WidgetAppListAdapter(data.widgetData)
     mainWidgetAppList.isNestedScrollingEnabled = false
+
+    itemView.setOnClickListener {
+      val intent = Intent(itemView.context, ConfigureActivity::class.java)
+          .putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, data.widget.appWidgetId)
+      itemView.context.startActivity(intent)
+    }
   }                                                       
 }
 
