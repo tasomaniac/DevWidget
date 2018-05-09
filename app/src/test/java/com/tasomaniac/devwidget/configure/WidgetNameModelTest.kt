8ed1dc205266b01d3c04391e63ad98d90232a73d
Tahsin Dane <sdane@wayfair.com>
2018-05-10 00:06:17 +0200

    Fix the widgetNameModel test

--- app/src/test/java/com/tasomaniac/devwidget/configure/WidgetNameModelTest.kt
+++ app/src/test/java/com/tasomaniac/devwidget/configure/WidgetNameModelTest.kt
@@ -1,12 +1,15 @@
 package com.tasomaniac.devwidget.configure
 
+import android.appwidget.AppWidgetManager
+import android.os.Bundle
+import android.widget.RemoteViews
 import com.nhaarman.mockito_kotlin.*
 import com.tasomaniac.devwidget.data.Widget
 import com.tasomaniac.devwidget.data.WidgetDao
 import com.tasomaniac.devwidget.rx.emptyDebouncer
 import com.tasomaniac.devwidget.rx.testScheduling
+import com.tasomaniac.devwidget.widget.RemoveViewsCreator
 import com.tasomaniac.devwidget.widget.WidgetUpdater
-import io.reactivex.Completable
 import io.reactivex.Maybe
 import org.junit.Test
 
@@ -15,9 +18,13 @@ class WidgetNameModelTest {
     private val widgetDao = mock<WidgetDao> {
         on { findWidgetById(APP_WIDGET_ID) } doReturn Maybe.empty()
     }
-    private val widgetUpdater = mock<WidgetUpdater> {
-        on { update(any()) } doReturn Completable.complete()
+    private val appWidgetManager = mock<AppWidgetManager> {
+        on { getAppWidgetOptions(APP_WIDGET_ID) } doReturn mock<Bundle>()
     }
+    private val removeViewsCreator = mock<RemoveViewsCreator> {
+        on { create(any(), any()) } doReturn mock<RemoteViews>()
+    }
+    private val widgetUpdater = WidgetUpdater(mock(), appWidgetManager, removeViewsCreator, widgetDao)
 
     private val useCase = WidgetNameModel(
         widgetDao,
@@ -34,39 +41,23 @@ class WidgetNameModelTest {
         useCase.updateWidgetName(ANY_WIDGET_NAME)
 
         then(widgetDao).should().insertWidgetSync(Widget(APP_WIDGET_ID))
-        then(widgetDao).should().updateWidgetSync(
-            Widget(
-                APP_WIDGET_ID,
-                ANY_WIDGET_NAME
-            )
-        )
+        then(widgetDao).should().updateWidgetSync(Widget(APP_WIDGET_ID, ANY_WIDGET_NAME))
+        then(appWidgetManager).should().updateAppWidget(eq(APP_WIDGET_ID), any())
     }
 
     @Test
     fun `given already available, should update widget`() {
-        given(widgetDao.findWidgetById(APP_WIDGET_ID)).willReturn(
-            Maybe.just(
-                ANY_WIDGET
-            )
-        )
+        given(widgetDao.findWidgetById(APP_WIDGET_ID)).willReturn(Maybe.just(ANY_WIDGET))
 
         useCase.updateWidgetName(ANY_WIDGET_NAME)
 
-        then(widgetDao).should().updateWidgetSync(
-            Widget(
-                APP_WIDGET_ID,
-                ANY_WIDGET_NAME
-            )
-        )
+        then(widgetDao).should().updateWidgetSync(Widget(APP_WIDGET_ID, ANY_WIDGET_NAME))
+        then(appWidgetManager).should().updateAppWidget(eq(APP_WIDGET_ID), any())
     }
 
     @Test
     fun `given already available, should emit current widget name`() {
-        given(widgetDao.findWidgetById(APP_WIDGET_ID)).willReturn(
-            Maybe.just(
-                ANY_WIDGET
-            )
-        )
+        given(widgetDao.findWidgetById(APP_WIDGET_ID)).willReturn(Maybe.just(ANY_WIDGET))
 
         useCase.currentWidgetName()
             .test()
@@ -77,9 +68,6 @@ class WidgetNameModelTest {
 
         private const val APP_WIDGET_ID = 1
         private const val ANY_WIDGET_NAME = "any_name"
-        private val ANY_WIDGET = Widget(
-            APP_WIDGET_ID,
-            ANY_WIDGET_NAME
-        )
+        private val ANY_WIDGET = Widget(APP_WIDGET_ID, ANY_WIDGET_NAME)
     }
 }
