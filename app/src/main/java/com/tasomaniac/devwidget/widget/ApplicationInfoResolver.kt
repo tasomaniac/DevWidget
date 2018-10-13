6b49a667116652aa930e07e39d706919d07defc4
Said Tahsin Dane <tasomaniac@gmail.com>
2018-10-13 21:36:22 +0300

    If no Launcher Activity found, put the application to the list.

--- app/src/main/java/com/tasomaniac/devwidget/widget/ApplicationInfoResolver.kt
+++ app/src/main/java/com/tasomaniac/devwidget/widget/ApplicationInfoResolver.kt
@@ -1,17 +1,22 @@
 package com.tasomaniac.devwidget.widget
 
+import android.content.pm.ApplicationInfo
 import android.content.pm.LauncherActivityInfo
 import android.content.pm.LauncherApps
 import android.content.pm.PackageManager
 import android.os.UserHandle
 import android.os.UserManager
+import com.tasomaniac.devwidget.data.AppDao
 import javax.inject.Inject
 
 class ApplicationInfoResolver @Inject constructor(
     private val packageManager: PackageManager,
     private val userManager: UserManager,
-    private val launcherApps: LauncherApps
+    private val launcherApps: LauncherApps,
+    private val appDao: AppDao
 ) {
+
+    private val mainUser = userManager.userProfiles[0]
 
     fun resolve(packageName: String): List<DisplayApplicationInfo> {
         return userManager.userProfiles
@@ -25,6 +30,23 @@ class ApplicationInfoResolver @Inject constructor(
             .getActivityList(packageName, user)
             .firstOrNull()
             ?.toDisplayApplicationInfo()
+            ?: resolveNoLauncherAppForMainUser(user, packageName)
+    }
+
+    private fun resolveNoLauncherAppForMainUser(
+        user: UserHandle,
+        packageName: String
+    ): DisplayApplicationInfo? {
+        return if (user == mainUser) {
+            try {
+                packageManager.getApplicationInfo(packageName, 0).toDisplayApplicationInfo()
+            } catch (e: PackageManager.NameNotFoundException) {
+                appDao.deleteAppSync(packageName)
+                null
+            }
+        } else {
+            null
+        }
     }
 
     private fun LauncherActivityInfo.toDisplayApplicationInfo() =
@@ -33,5 +55,13 @@ class ApplicationInfoResolver @Inject constructor(
             applicationInfo.packageName,
             getBadgedIcon(0),
             user
+        )
+
+    private fun ApplicationInfo.toDisplayApplicationInfo() =
+        DisplayApplicationInfo(
+            packageManager.getApplicationLabel(this),
+            packageName,
+            loadIcon(packageManager),
+            mainUser
         )
 }
