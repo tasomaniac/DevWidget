bdf8305f455b21ed68b74a538a7ea8d9f9eaeed2
Said Tahsin Dane <tasomaniac@gmail.com>
2018-10-14 15:13:48 +0300

    Move custom view to package where it is used

--- app/src/main/java/com/tasomaniac/devwidget/main/ForegroundLinearLayout.java
+++ app/src/main/java/com/tasomaniac/devwidget/main/ForegroundLinearLayout.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.tasomaniac.devwidget.view;
+package com.tasomaniac.devwidget.main;
 
 import android.content.Context;
 import android.content.res.TypedArray;
