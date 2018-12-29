de8c5208c96e0e19086f1c7783e4bb0ddd0e8f6b
Said Tahsin Dane <tasomaniac@gmail.com>
2018-12-29 22:54:32 +0300

    Make widget preview more realistic

--- widget-preview/src/main/kotlin/com/tasomaniac/devwidget/widget/preview/WidgetView.kt
+++ widget-preview/src/main/kotlin/com/tasomaniac/devwidget/widget/preview/WidgetView.kt
@@ -1,27 +1,35 @@
 package com.tasomaniac.devwidget.widget.preview
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.tasomaniac.devwidget.widget.WidgetResources
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.widget_preview_layout.mainWidgetAppList
-import kotlinx.android.synthetic.main.widget_preview_layout.mainWidgetTitle
+import kotlinx.android.synthetic.main.widget_preview_layout.widgetPreviewAppList
 import javax.inject.Inject
 
-class WidgetView internal constructor(
-    private val widgetNameResolver: WidgetNameResolver,
+class WidgetView private constructor(
     private val appViewHolderFactory: AppViewHolder.Factory,
+    private val widgetResources: WidgetResources,
     override val containerView: View
 ) : LayoutContainer {
 
+    private val widgetTitle = containerView.findViewById<TextView>(R.id.widgetTitle)
+    private val widgetDevOptions = containerView.findViewById<ImageView>(R.id.widgetDevOptions)
+    private val widgetConfigure = containerView.findViewById<ImageView>(R.id.widgetConfigure)
+
     fun bind(data: WidgetListData) {
-        mainWidgetTitle.text = widgetNameResolver.resolve(data.widget)
-        mainWidgetAppList.adapter = AppListAdapter(appViewHolderFactory, data.apps, data.favAction)
+        widgetTitle.text = data.widget.name
+        widgetDevOptions.setImageResource(widgetResources.devOptionsIcon)
+        widgetConfigure.setImageResource(widgetResources.settingsIcon)
+        widgetPreviewAppList.adapter = AppListAdapter(appViewHolderFactory, data.apps, data.favAction)
     }
 
     class Factory @Inject internal constructor(
-        private val widgetNameResolver: WidgetNameResolver,
-        private val appViewHolderFactory: AppViewHolder.Factory
+        private val appViewHolderFactory: AppViewHolder.Factory,
+        private val widgetResources: WidgetResources
     ) {
 
-        fun createWith(view: View) = WidgetView(widgetNameResolver, appViewHolderFactory, view)
+        fun createWith(view: View) = WidgetView(appViewHolderFactory, widgetResources, view)
     }
 }
