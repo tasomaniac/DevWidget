87eb6eaf3d844368f225adbad503a3015f72488b
Said Tahsin Dane <tasomaniac@gmail.com>
2019-04-06 18:40:49 +0200

    update dependencies

--- data/src/main/kotlin/com/tasomaniac/devwidget/data/WidgetDao.kt
+++ data/src/main/kotlin/com/tasomaniac/devwidget/data/WidgetDao.kt
@@ -3,11 +3,11 @@ package com.tasomaniac.devwidget.data
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.OnConflictStrategy.ABORT
+import androidx.room.OnConflictStrategy.IGNORE
 import androidx.room.Query
 import androidx.room.Update
 import io.reactivex.Completable
-
 import io.reactivex.Maybe
 import io.reactivex.Single
 
@@ -23,10 +23,10 @@ interface WidgetDao {
     @Query("SELECT * from widget where appWidgetId IN (:appWidgetId)")
     fun findWidgetsById(appWidgetId: IntArray): Single<List<Widget>>
 
-    @Insert(onConflict = OnConflictStrategy.FAIL)
+    @Insert(onConflict = ABORT)
     fun insertWidget(vararg widget: Widget): Completable
 
-    @Update(onConflict = OnConflictStrategy.IGNORE)
+    @Update(onConflict = IGNORE)
     fun updateWidget(widget: Widget): Completable
 
     @Query("UPDATE widget SET appWidgetId = :appWidgetId WHERE appWidgetId = -1")
